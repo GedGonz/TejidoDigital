7a4e15012032f09925f86dce046ab3c700745e31
Gerald Gonzalez <gedgonz7@gmail.com>
2016-02-25 23:39:23 -0600

    upload image in folder server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var mongoose= require('mongoose');/*Importamos la libreria mongodb*/
 var router = express.Router();
+var multer = require('multer');
+var nameimage="";
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+     var tipo=file.mimetype;
+ if(tipo=='image/png' || tipo=='image/jpg' || tipo=='image/jpeg')
+ {
+  console.log(file.originalname);
+    callback(null, './public/uploads/');
+ }
+
+     
+    
+  },
+  filename: function (req, file, callback) {
+      nameimage=file.originalname;
+    callback(null, file.originalname);
+  }
+});
+
+//var upload = multer({ storage : storage}).single('image');
+
 
 /*Coneccion a Monogodb*/
+//mongodb://ged:gedgonz791@ds015478.mongolab.com:15478/tejido
 mongoose.connect("mongodb://localhost/tejido");
 
 //Definir nuestro esquema de la Tabla Tencnologias
@@ -33,24 +56,30 @@ router.get('/servicios', function(req, res, next) {
 router.get('/servicios/new', function(req, res, next) {
   res.render('services/new', { title: 'Express' });
 });
-
-router.post('/servicios/save', function(req, res, next) {
+//var fs=require('fs');
+router.post('/servicios/save',multer({ storage : storage}).single('image'),function(req, res, next) {
  
+
+
+
+
   var data=
   {
   Titulo:req.body.titulo,
   Descripcion:req.body.descripcion,
-  ImageUrl:req.body.image,
+  ImageUrl:nameimage,
   Precio:req.body.precio
   }
+
+
   var Tecno=new Tecnologias(data);
   Tecno.save(function(err)
   {
     console.log(Tecno);
   })
 
-  
-  res.render('index', { title: 'Express' });
+
+  res.render('index');
 });
 
 
